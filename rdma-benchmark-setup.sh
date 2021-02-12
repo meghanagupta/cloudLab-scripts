7b465f0660e3625775d8123549b591e9c1137498
Meghana Gupta <meghanagupta3@gmail.com>
2021-02-11 23:57:57 -0500

    Update rdma-benchmark-setup.sh

--- rdma-benchmark-setup.sh
+++ rdma-benchmark-setup.sh
@@ -8,18 +8,20 @@ cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 644 ~/.ssh/authorized_keys
 
 # Install packages
-apt update
-apt --assume-yes install mosh vim tmux pdsh tree axel python3 python3-pip
-apt --assume-yes install linux-tools-common linux-tools-${kernel_release} hugepages cpuset msr-tools i7z
+sudo apt update
+sudo apt --assume-yes install mosh vim tmux pdsh tree axel python3 python3-pip
+sudo apt --assume-yes install linux-tools-common linux-tools-${kernel_release} hugepages cpuset msr-tools i7z
 
 if [ -f /local/startup_service_done ]; then
     exit 0
 fi
 
 kernel_boot_params="intel_iommu=on iommu=pt"
-sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"$kernel_boot_params /" /etc/default/grub
-update-grub
+sudo sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"$kernel_boot_params /" /etc/default/grub
+sudo update-grub
 
+sudo -s
 > /local/startup_service_done
+exit
 
-reboot
+sudo reboot
